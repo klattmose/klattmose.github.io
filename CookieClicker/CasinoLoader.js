88dcd250e4bbf840713d34fadeca3314bf99af8c
klattmose <mattklose@gmail.com>
2019-04-03 15:37:08 -0400

    Update CasinoLoader.js

--- CookieClicker/CasinoLoader.js
+++ CookieClicker/CasinoLoader.js
@@ -1,8 +1,8 @@
-Game.LoadMinigameMod = function(objKey, miniName, miniSource){
+/*Game.LoadMinigameMod = function(objKey, miniName, miniSource){
 	Game.Objects[objKey].minigameUrl = miniSource;
 	Game.Objects[objKey].minigameName = miniName;
 	Game.LoadMinigames();
 }
 
 Game.Win('Third-party');
-Game.LoadMinigameMod('Chancemaker', 'Casino', 'https://klattmose.github.io/CookieClicker/minigameCasino/minigameCasino.js');
+Game.LoadMinigameMod('Chancemaker', 'Casino', 'https://klattmose.github.io/CookieClicker/minigameCasino/minigameCasino.js?c');*/

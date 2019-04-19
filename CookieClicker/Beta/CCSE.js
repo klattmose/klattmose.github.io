2bb88aedde35084d0d7f7993548e0aca735bb2cc
klattmose <mattklose@gmail.com>
2019-04-18 22:07:29 -0400

    Update CCSE.js

--- CookieClicker/Beta/CCSE.js
+++ CookieClicker/Beta/CCSE.js
@@ -661,6 +661,21 @@ CCSE.launch = function(){
 		
 		// -----     GRANDMAPOCALYPSE block     ----- //
 		
+		// I need this because this gets used once and if I leave it out the game breaks
+		function inRect(x,y,rect)
+		{
+			//find out if the point x,y is in the rotated rectangle rect{w,h,r,o} (width,height,rotation in radians,y-origin) (needs to be normalized)
+			//I found this somewhere online I guess
+			var dx = x+Math.sin(-rect.r)*(-(rect.h/2-rect.o)),dy=y+Math.cos(-rect.r)*(-(rect.h/2-rect.o));
+			var h1 = Math.sqrt(dx*dx + dy*dy);
+			var currA = Math.atan2(dy,dx);
+			var newA = currA - rect.r;
+			var x2 = Math.cos(newA) * h1;
+			var y2 = Math.sin(newA) * h1;
+			if (x2 > -0.5 * rect.w && x2 < 0.5 * rect.w && y2 > -0.5 * rect.h && y2 < 0.5 * rect.h) return true;
+			return false;
+		}
+		
 		// Game.UpdateGrandmapocalypse
 		// executed every logic frame
 		if(!Game.customUpdateGrandmapocalypse) Game.customUpdateGrandmapocalypse = [];

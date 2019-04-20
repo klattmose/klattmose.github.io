02607596072aa1c45eea4403bbf9a9ea95f4bbae
klattmose <mattklose@gmail.com>
2019-04-20 13:17:42 -0400

    Update TimerWidget.js

--- CookieClicker/TimerWidget.js
+++ CookieClicker/TimerWidget.js
@@ -3,12 +3,14 @@ if(TimerWidget === undefined) var TimerWidget = {};
 if(typeof CCSE == 'undefined') Game.LoadMod('https://klattmose.github.io/CookieClicker/CCSE.js');
 TimerWidget.pic = 'https://klattmose.github.io/CookieClicker/img/timer.png';
 TimerWidget.name = 'Timer Widget';
-TimerWidget.version = '1.0';
+TimerWidget.version = '1.1';
 TimerWidget.GameVersion = '2.019';
 
 TimerWidget.launch = function(){
 	TimerWidget.init = function(){
-		TimerWidget.buffColors = {
+		TimerWidget.colorLookup = {
+			'golden': 'Purple', 
+			'reindeer': 'Orange', 
 			'Frenzy': 'Yellow', 
 			'Dragon Harvest': 'Brown', 
 			'Elder frenzy': 'Green', 
@@ -35,6 +37,10 @@ TimerWidget.launch = function(){
 		Game.customToggleSpecialMenu.push(TimerWidget.ToggleSpecialMenu);
 		Game.customDrawSpecial.push(TimerWidget.Update);
 		
+		Game.customLoad.push(function(){
+			l('specialPopup').className='framed prompt offScreen';
+		});
+		
 		TimerWidget.isLoaded = 1;
 	}
 	
@@ -51,90 +57,80 @@ TimerWidget.launch = function(){
 		return str;
 	}
 	
+	TimerWidget.bar = function(name, bars, time) {
+		// Put this here so we don't keep redefining it
+		
+		var div = document.createElement('div');
+		div.style.width = '100%';
+		div.style.height = '10px';
+		div.style.margin = 'auto';
+		div.style.position = 'absolute';
+		div.style.left = '0px';
+		div.style.top = '0px';
+		div.style.right = '0px';
+		div.style.bottom = '0px';
+
+		var type = document.createElement('span');
+		type.style.display = 'inline-block';
+		type.style.textAlign = 'right';
+		type.style.width = '108px';
+		type.style.marginRight = '5px';
+		type.style.verticalAlign = 'text-top';
+		type.textContent = name;
+		div.appendChild(type);
+
+		for (var i = 0; i < bars.length; i++) {
+			var colorBar = document.createElement('span');
+			colorBar.id = bars[i].id
+			colorBar.style.display = 'inline-block';
+			colorBar.style.height = '10px';
+			if (bars.length - 1 == i) {
+				colorBar.style.borderTopRightRadius = '10px';
+				colorBar.style.borderBottomRightRadius = '10px';
+			}
+			if (typeof bars[i].color !== 'undefined') {
+				colorBar.style.backgroundColor = bars[i].color;
+			}
+			div.appendChild(colorBar);
+		}
+
+		var timer = document.createElement('span');
+		timer.id = time;
+		timer.style.marginLeft = '5px';
+		timer.style.verticalAlign = 'text-top';
+		div.appendChild(timer);
+		return div
+	}
+	
 	TimerWidget.Update = function(){
 		if(Game.specialTab == 'timer'){
 			var maxWidth = l('TimerBar').getBoundingClientRect().width - 159;
-			var bar = function(name, bars, time) {
-				var div = document.createElement('div');
-				div.style.width = '100%';
-				div.style.height = '10px';
-				div.style.margin = 'auto';
-				div.style.position = 'absolute';
-				div.style.left = '0px';
-				div.style.top = '0px';
-				div.style.right = '0px';
-				div.style.bottom = '0px';
-
-				var type = document.createElement('span');
-				type.style.display = 'inline-block';
-				type.style.textAlign = 'right';
-				type.style.width = '108px';
-				type.style.marginRight = '5px';
-				type.style.verticalAlign = 'text-top';
-				type.textContent = name;
-				div.appendChild(type);
-
-				for (var i = 0; i < bars.length; i++) {
-					var colorBar = document.createElement('span');
-					colorBar.id = bars[i].id
-					colorBar.style.display = 'inline-block';
-					colorBar.style.height = '10px';
-					if (bars.length - 1 == i) {
-						colorBar.style.borderTopRightRadius = '10px';
-						colorBar.style.borderBottomRightRadius = '10px';
-					}
-					if (typeof bars[i].color !== 'undefined') {
-						colorBar.style.backgroundColor = bars[i].color;
-					}
-					div.appendChild(colorBar);
-				}
-
-				var timer = document.createElement('span');
-				timer.id = time;
-				timer.style.marginLeft = '5px';
-				timer.style.verticalAlign = 'text-top';
-				div.appendChild(timer);
-				return div
-			}
 			
 			l('TimerBar').innerHTML = '';
 			
-			if(Game.shimmerTypes["golden"].spawnConditions() && Game.shimmerTypes['golden'].spawned == 0){
-				var TimerBarGC = document.createElement('div');
-				TimerBarGC.id = 'TimerBarGC';
-				TimerBarGC.style.height = '12px';
-				TimerBarGC.style.margin = '0px 10px';
-				TimerBarGC.style.position = 'relative';
-				TimerBarGC.appendChild(bar('Next Cookie', [{id: 'TimerBarGCMinBar', color: 'Gray'}, {id: 'TimerBarGCBar', color: 'Purple'}], 'TimerBarGCTime'));
-				l('TimerBar').appendChild(TimerBarGC);
-				
-				TimerBarGC.style.display = '';
-				l('TimerBarGCMinBar').style.width = Math.round(Math.max(0, Game.shimmerTypes['golden'].minTime - Game.shimmerTypes['golden'].time) * maxWidth / Game.shimmerTypes['golden'].maxTime) + 'px';
-				if (Game.shimmerTypes['golden'].minTime == Game.shimmerTypes['golden'].maxTime) {
-					l('TimerBarGCMinBar').style.borderTopRightRadius = '10px';
-					l('TimerBarGCMinBar').style.borderBottomRightRadius = '10px';
+			for(var key in Game.shimmerTypes){
+				if(Game.shimmerTypes[key].spawnConditions() && Game.shimmerTypes[key].spawned == 0 && Game.shimmerTypes[key].spawnsOnTimer){
+					var TimerBarShimmer = document.createElement('div');
+					TimerBarShimmer.id = 'TimerBar' + key;
+					TimerBarShimmer.style.height = '12px';
+					TimerBarShimmer.style.margin = '0px 10px';
+					TimerBarShimmer.style.position = 'relative';
+					TimerBarShimmer.appendChild(TimerWidget.bar('Next ' + key, [{id: key + 'TBMinBar', color: 'Gray'}, {id: key + 'TBBar', color: TimerWidget.colorLookup[key]}], key + 'TBTime'));
+					l('TimerBar').appendChild(TimerBarShimmer);
+					
+					TimerBarShimmer.style.display = '';
+					l(key + 'TBMinBar').style.width = Math.round(Math.max(0, Game.shimmerTypes[key].minTime - Game.shimmerTypes[key].time) * maxWidth / Game.shimmerTypes[key].maxTime) + 'px';
+					if (Game.shimmerTypes[key].minTime == Game.shimmerTypes[key].maxTime) {
+						l(key + 'TBMinBar').style.borderTopRightRadius = '10px';
+						l(key + 'TBMinBar').style.borderBottomRightRadius = '10px';
+					}
+					else {
+						l(key + 'TBMinBar').style.borderTopRightRadius = '';
+						l(key + 'TBMinBar').style.borderBottomRightRadius = '';
+					}
+					l(key + 'TBBar').style.width = Math.round(Math.min(Game.shimmerTypes[key].maxTime - Game.shimmerTypes[key].minTime, Game.shimmerTypes[key].maxTime - Game.shimmerTypes[key].time) * maxWidth / Game.shimmerTypes[key].maxTime) + 'px';
+					l(key + 'TBTime').textContent = Math.ceil((Game.shimmerTypes[key].maxTime - Game.shimmerTypes[key].time) / Game.fps);
 				}
-				else {
-					l('TimerBarGCMinBar').style.borderTopRightRadius = '';
-					l('TimerBarGCMinBar').style.borderBottomRightRadius = '';
-				}
-				l('TimerBarGCBar').style.width = Math.round(Math.min(Game.shimmerTypes['golden'].maxTime - Game.shimmerTypes['golden'].minTime, Game.shimmerTypes['golden'].maxTime - Game.shimmerTypes['golden'].time) * maxWidth / Game.shimmerTypes['golden'].maxTime) + 'px';
-				l('TimerBarGCTime').textContent = Math.ceil((Game.shimmerTypes['golden'].maxTime - Game.shimmerTypes['golden'].time) / Game.fps);
-			}
-			
-			if(Game.shimmerTypes["reindeer"].spawnConditions() && Game.shimmerTypes['reindeer'].spawned == 0){
-				var TimerBarRen = document.createElement('div');
-				TimerBarRen.id = 'TimerBarRen';
-				TimerBarRen.style.height = '12px';
-				TimerBarRen.style.margin = '0px 10px';
-				TimerBarRen.style.position = 'relative';
-				TimerBarRen.appendChild(bar('Next Reindeer', [{id: 'TimerBarRenMinBar', color: 'Gray'}, {id: 'TimerBarRenBar', color: 'Orange'}], 'TimerBarRenTime'));
-				l('TimerBar').appendChild(TimerBarRen);
-				
-				TimerBarRen.style.display = '';
-				l('TimerBarRenMinBar').style.width = Math.round(Math.max(0, Game.shimmerTypes['reindeer'].minTime - Game.shimmerTypes['reindeer'].time) * maxWidth / Game.shimmerTypes['reindeer'].maxTime) + 'px';
-				l('TimerBarRenBar').style.width = Math.round(Math.min(Game.shimmerTypes['reindeer'].maxTime - Game.shimmerTypes['reindeer'].minTime, Game.shimmerTypes['reindeer'].maxTime - Game.shimmerTypes['reindeer'].time) * maxWidth / Game.shimmerTypes['reindeer'].maxTime) + 'px';
-				l('TimerBarRenTime').textContent = Math.ceil((Game.shimmerTypes['reindeer'].maxTime - Game.shimmerTypes['reindeer'].time) / Game.fps);
 			}
 			
 			for(var i in Game.buffs){
@@ -144,15 +140,15 @@ TimerWidget.launch = function(){
 				TimerBarBuff.style.height = '12px';
 				TimerBarBuff.style.margin = '0px 10px';
 				TimerBarBuff.style.position = 'relative';
-				TimerBarBuff.appendChild(bar('', [{id: TimerBarBuff.id + 'Bar'}], TimerBarBuff.id + 'Time'));
+				TimerBarBuff.appendChild(TimerWidget.bar('', [{id: TimerBarBuff.id + 'Bar'}], TimerBarBuff.id + 'Time'));
 				TimerBarBuff.firstChild.firstChild.id = TimerBarBuff.id + 'Type';
 				l('TimerBar').appendChild(TimerBarBuff);
 				
 				TimerBarBuff.style.display = '';
 				l(TimerBarBuff.id + 'Type').textContent = buff.name;
 				var classColor = '';
-				if (typeof TimerWidget.buffColors[Game.buffs[i].name] !== 'undefined') {
-					classColor = TimerWidget.buffColors[Game.buffs[i].name];
+				if (typeof TimerWidget.colorLookup[Game.buffs[i].name] !== 'undefined') {
+					classColor = TimerWidget.colorLookup[Game.buffs[i].name];
 				}
 				else {
 					classColor = 'Purple';

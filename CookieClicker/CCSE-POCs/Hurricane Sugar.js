4de91fa4140b635c654b650ea8aa5ca11461c567
klattmose <mattklose@gmail.com>
2019-04-27 17:57:29 -0400

    hurricane sugar

---
+++ CookieClicker/CCSE-POCs/Hurricane Sugar.js
@@ -0,0 +1,77 @@
+Game.Win('Third-party');
+if(HurricaneSugar === undefined) var HurricaneSugar = {};
+if(typeof CCSE == 'undefined') Game.LoadMod('https://klattmose.github.io/CookieClicker/' + (1 ? 'Beta/' : '') + 'CCSE.js');
+HurricaneSugar.name = 'Hurricane Sugar';
+HurricaneSugar.version = '0.2';
+HurricaneSugar.GameVersion = '2.019';
+
+HurricaneSugar.launch = function(){
+	HurricaneSugar.init = function(){
+		CCSE.NewBuff('hurricane sugar',function(time, pow){
+			return {
+				name: 'Hurri-Cane Sugar',
+				desc: 'Sugar lumps ripen in ' + Game.sayTime(pow * 2 / 1000 * Game.fps, -1) + ' for ' + Game.sayTime(time * Game.fps, -1) + '!',
+				icon: [29,14],
+				time: time * Game.fps,
+				add: true,
+				power: pow
+			};
+		});
+		
+		Game.customComputeLumpTimes.push(HurricaneSugar.buffedLumpTime);
+		HurricaneSugar.InjectIntoGoldenCookie();
+		CCSE.MinigameReplacer(HurricaneSugar.InjectIntoHandofFate, 'Wizard tower');
+		
+		
+		HurricaneSugar.isLoaded = 1;
+	}
+	
+	HurricaneSugar.buffedLumpTime = function(){
+		if(Game.hasBuff('Hurri-Cane Sugar')){
+			var buff = Game.buffs['Hurri-Cane Sugar'];
+			Game.lumpMatureAge = buff.power;
+			Game.lumpRipeAge = buff.power * 2;
+			Game.lumpOverripeAge = buff.power * 10;
+		}
+	}
+	
+	HurricaneSugar.InjectIntoGoldenCookie = function(){
+		Game.customShimmerTypes['golden'].customListPush.push(function(me, list){
+			if(me.wrath == 0 && Game.canLumps() && Math.random() < 0.1) list.push('hurricane sugar');
+		});
+		
+		Game.customShimmerTypes['golden'].customBuff.push(function(me, buff, choice, effectDurMod, mult){
+			console.log(me, buff, choice, effectDurMod, mult);
+			var ret = buff;
+			if(choice == 'hurricane sugar') ret = Game.gainBuff('hurricane sugar', Math.ceil(5 * effectDurMod), 500);
+			return ret;
+		});
+	}
+	
+	HurricaneSugar.InjectIntoHandofFate = function(){
+		Game.customMinigame['Wizard tower'].fateWin.push(function(choices){
+			if(Math.random() < 0.1) choices.push('hurricane sugar');
+		});
+		
+		if(typeof FortuneCookie == 'undefined') FortuneCookie = {};
+		if(FortuneCookie.customFateCheckerWin === undefined) FortuneCookie.customFateCheckerWin = [];
+		FortuneCookie.customFateCheckerWin.push(function(spellCount, idx, choices){
+			if(Math.random() < 0.1) choices.push('Hurri-Cane Sugar');
+		});
+	}
+	
+	
+	HurricaneSugar.init();
+}
+
+
+if(!HurricaneSugar.isLoaded){
+	if(CCSE && CCSE.isLoaded){
+		HurricaneSugar.launch();
+	}
+	else{
+		if(!CCSE) var CCSE = {};
+		if(!CCSE.postLoadHooks) CCSE.postLoadHooks = [];
+		CCSE.postLoadHooks.push(HurricaneSugar.launch);
+	}
+}

c1eb3f6529f505730deb0ad7222598b76c54dd96
klattmose <mattklose@gmail.com>
2019-05-11 12:57:05 -0400

    Oops, broke wrinklers

--- CookieClicker/CCSE.js
+++ CookieClicker/CCSE.js
@@ -986,21 +986,6 @@ CCSE.launch = function(){
 		
 		// -----     GRANDMAPOCALYPSE block     ----- //
 		
-		// I need this because this gets used once and if I leave it out the game breaks
-		function inRect(x,y,rect)
-		{
-			//find out if the point x,y is in the rotated rectangle rect{w,h,r,o} (width,height,rotation in radians,y-origin) (needs to be normalized)
-			//I found this somewhere online I guess
-			var dx = x+Math.sin(-rect.r)*(-(rect.h/2-rect.o)),dy=y+Math.cos(-rect.r)*(-(rect.h/2-rect.o));
-			var h1 = Math.sqrt(dx*dx + dy*dy);
-			var currA = Math.atan2(dy,dx);
-			var newA = currA - rect.r;
-			var x2 = Math.cos(newA) * h1;
-			var y2 = Math.sin(newA) * h1;
-			if (x2 > -0.5 * rect.w && x2 < 0.5 * rect.w && y2 > -0.5 * rect.h && y2 < 0.5 * rect.h) return true;
-			return false;
-		}
-		
 		// Game.UpdateGrandmapocalypse
 		// executed every logic frame
 		if(!Game.customUpdateGrandmapocalypse) Game.customUpdateGrandmapocalypse = [];
@@ -1025,6 +1010,7 @@ CCSE.launch = function(){
 		
 		
 		// Game.UpdateWrinklers
+		// Need to define this damn function or wrinklers break
 		// customWrinklerSpawnChance functions should return a multiplier to chance. (Return 1 to have no effect)
 		if(!Game.customUpdateWrinklers) Game.customUpdateWrinklers = [];
 		if(!Game.customWrinklerSpawnChance) Game.customWrinklerSpawnChance = [];
@@ -1037,7 +1023,20 @@ CCSE.launch = function(){
 					for(var i in Game.customWrinklerPop) Game.customWrinklerPop[i](me);`, -1);
 		CCSE.SpliceCodeIntoFunction('Game.UpdateWrinklers', -1, `
 			// Game.UpdateWrinklers injection point 2
-			for(var i in Game.customUpdateWrinklers) Game.customUpdateWrinklers[i]();`);
+			for(var i in Game.customUpdateWrinklers) Game.customUpdateWrinklers[i]();`, 
+			`function inRect(x,y,rect)
+			{
+				//find out if the point x,y is in the rotated rectangle rect{w,h,r,o} (width,height,rotation in radians,y-origin) (needs to be normalized)
+				//I found this somewhere online I guess
+				var dx = x+Math.sin(-rect.r)*(-(rect.h/2-rect.o)),dy=y+Math.cos(-rect.r)*(-(rect.h/2-rect.o));
+				var h1 = Math.sqrt(dx*dx + dy*dy);
+				var currA = Math.atan2(dy,dx);
+				var newA = currA - rect.r;
+				var x2 = Math.cos(newA) * h1;
+				var y2 = Math.sin(newA) * h1;
+				if (x2 > -0.5 * rect.w && x2 < 0.5 * rect.w && y2 > -0.5 * rect.h && y2 < 0.5 * rect.h) return true;
+				return false;
+			}`);
 		
 		
 		// Game.DrawWrinklers

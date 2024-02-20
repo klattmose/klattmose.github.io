7c19bbbbf2469ad450fa49436fb26b029e1dc7ec
klattmose <mattklose@gmail.com>
2024-02-20 11:02:20 -0500

    Update main.js

--- CookieClicker/SteamMods/SteamAchievementEnabler/main.js
+++ CookieClicker/SteamMods/SteamAchievementEnabler/main.js
@@ -1,8 +1,10 @@
 Game.registerMod("Steam Achievement Enabler",{
 	init:function(){
 		if(Steam){
-			Steam.allowSteamAchievs = true;
-			//eval('Steam.justLoadedSave=' + Steam.justLoadedSave.toString().replace("Game.HasAchiev('Cheated cookies taste awful')", false));
+			if(!Steam.allowSteamAchievs){
+				Steam.allowSteamAchievs = true;
+				Steam.justLoadedSave();
+			}
 		}
 	}
 });
